0b11e00cfcb1c6f640b591d980f6ef8dd46bdd50
SuzanneJokhu <[sjokhu25@gmail.com]>
2024-10-04 13:03:43 -0400

    removed jobzilla from this folder

--- jobzilla/blogs/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . import models
-
-
-class CreateBlog(forms.ModelForm):
-    class Meta:
-        model = models.Blog
-        fields = ["title", "body", "slug", "link", "banner"]
--- jobzilla/jobs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "jobs"
-
-urlpatterns = [
-    path("", views.jobs_list, name="joblist"),
-    path("new-job/", views.job_new, name="new-job"),
-]
